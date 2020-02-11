616c0c348572d925f2015a43fdce63296abd486b
KINGGUOKUN <184494760@qq.com>
2020-02-11 15:41:04 +0800

    基础架构

---
+++ SystemManagement.Common/BusinessException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemManagement.Common
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(int hResult, string message)
+            : base(message)
+        {
+            base.HResult = hResult;
+        }
+    }
+}
